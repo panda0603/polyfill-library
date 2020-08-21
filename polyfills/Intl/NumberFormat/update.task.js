a932fc022213972f0bc79b196d0825522421d629
Jake Champion <me@jakechampion.name>
2020-08-21 23:38:15 +0100

    Update update.task.js

--- polyfills/Intl/NumberFormat/update.task.js
+++ polyfills/Intl/NumberFormat/update.task.js
@@ -49,7 +49,7 @@ configSource.dependencies.push('Intl.NumberFormat');
 configSource.test = { ci: false };
 
 function intlLocaleDetectFor(locale) {
-	return "'Intl' in self && Intl.NumberFormat && (function () {\n\t\ttry {\n\t\t  new Intl.NumberFormat('".concat(locale, "', {\n\t\t\tstyle: 'unit',\n\t\t\tunit: 'byte'\n\t\t  });\n\t\t} catch (e) {\n\t\t  return false;\n\t\t}\n\t\treturn true;\n\t  })() && Intl.NumberFormat.supportedLocalesOf('").concat(locale, "').length");
+	return "'Intl' in self && Intl.NumberFormat && (function () {\n\t\ttry {\n\t\t\tnew Intl.NumberFormat('".concat(locale, "', {\n\t\t\tstyle: 'unit',\n\t\t\tunit: 'byte'\n\t\t  });\n\t\t} catch (e) {\n\t\t\treturn false;\n\t\t}\n\t\treturn true;\n\t  })() && Intl.NumberFormat.supportedLocalesOf('").concat(locale, "').length");
 }
 
 console.log('Importing Intl.NumberFormat~locale.* polyfill from ' + LocalesPath);
